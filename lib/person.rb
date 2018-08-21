0d84cf25c5e061257c2320921485271dbeace96f
Will Figueiredo <will@wfigueiredo.com>
2018-08-21 05:05:26 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,18 @@
 class Person
-
+  def instantiate
+    @names
+    @jobs
+  end
+  def names=(name)
+    @names = name
+  end
+  def name
+    @names
+  end
+  def jobs=(job)
+    @jobs = job
+  end
+  def job
+    @jobs
+  end
 end
